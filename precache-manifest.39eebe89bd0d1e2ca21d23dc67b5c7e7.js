f547322def7ad94b31cfba76b62f1589a80656f6
Patricio Lopez J <lopezjuripatricio@gmail.com>
2019-03-11 10:55:34 -0400

    Updates

---
+++ precache-manifest.39eebe89bd0d1e2ca21d23dc67b5c7e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbecb0ffd46eb2a7550e",
+    "url": "/react-metamask/static/js/runtime~main.9b7d2817.js"
+  },
+  {
+    "revision": "60cd8263648bf416c317",
+    "url": "/react-metamask/static/js/main.a9e86c81.chunk.js"
+  },
+  {
+    "revision": "a0c42cebfb455b6d8155",
+    "url": "/react-metamask/static/js/2.145666c0.chunk.js"
+  },
+  {
+    "revision": "d681823dfc15fd41d9438bca7ebe83cc",
+    "url": "/react-metamask/index.html"
+  }
+];
